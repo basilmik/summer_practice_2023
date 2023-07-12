365f35909e88be0df5d5f5995dcb096350f6a4c2
basil <basil@BASILIK>
2023-07-12 19:22:23 +0300

    task5 done

--- task_5/main.cpp
+++ task_5/main.cpp
@@ -1,48 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 #include "string.h"
-
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
-
 #include "../libwinbgi/src/graphics.h"
 
+#define X0 30
+#define Y0 30
+#define MAX_WIN_SIZE 1000
+#define MAX_RL_NUM 10
+
+typedef struct mdata_t
+{
+	int clr;
+	int val;
+
+} clr_int;
 
 class marr_data
 {
-public:
-	int* arr_all; int* arr1; int* arr2;
-
-	int size1;
-	int size2;
-	int size_all;
-
-	marr_data()
+private:
+	int is_size_valid()
 	{
-		size1 = -1;
-		size2 = -1;
-		size_all = -1;
-		arr1 = nullptr;
-		arr2 = nullptr;
-		arr_all = nullptr;
+		return (size1 > 0 && size2 > 0 && size_all > 0);
 	}
 
-	void mmerge(int arr[], int left, int mid, int right)
+	void new_msort(clr_int* arr, int left, int right)
+	{
+		if (left + 1 >= right)
+			return;
+
+		int mid = (left + right) / 2;
+		new_msort(arr, left, mid);
+		new_msort(arr, mid, right);
+
+		new_mmerge(arr, left, mid, right);
+	}
+
+	void new_mmerge(clr_int arr[], int left, int mid, int right)
 	{
 		int it1 = 0, it2 = 0;
 		int size = right - left;
-		int* res = new int[size];
+		clr_int* res = new clr_int[size];
 
 		for (int i = 0; i < size; i++)
-			res[i] = 0;
+			res[i].val = 0;
 
 
 		while (left + it1 < mid && mid + it2 < right)
 		{
-			if (arr[left + it1] < arr[mid + it2])
+			if (arr[left + it1].val < arr[mid + it2].val)
 			{
 				res[it1 + it2] = arr[left + it1];
 				it1++;
@@ -74,41 +83,26 @@ public:
 		}
 	}
 
-	void msort(int* arr, int left, int right)
+public:
+	clr_int* arr_all; int* arr1; int* arr2;
+
+	int size1;
+	int size2;
+	int size_all;
+
+	marr_data()
 	{
-		if (left + 1 >= right)
-			return;
-
-		int mid = (left + right) / 2;
-		msort(arr, left, mid);
-		msort(arr, mid, right);
-
-		mmerge(arr, left, mid, right);
+		size1 = -1;
+		size2 = -1;
+		size_all = -1;
+		arr1 = nullptr;
+		arr2 = nullptr;
+		arr_all = nullptr;
 	}
 
 	void sort_arr_all()
 	{
-		msort(arr_all, 0, size_all);
-	}
-
-	void print_arrs()
-	{
-		printf("arr1: ");
-		for (int i = 0; i < size1; i++)
-		{
-			printf("%d ", arr1[i]);
-		}
-		printf("\narr2: ");
-		for (int i = 0; i < size2; i++)
-		{
-			printf("%d ", arr2[i]);
-		}
-		printf("\narr_all: ");
-		for (int i = 0; i < size_all; i++)
-		{
-			printf("%d ", arr_all[i]);
-		}
-		printf("\n");
+		new_msort(arr_all, 0, size_all);
 	}
 
 	void set_size1(int _s)
@@ -131,13 +125,115 @@ public:
 
 		arr1 = new int[size1];
 		arr2 = new int[size2];
-		arr_all = new int[size_all];
+		arr_all = new clr_int[size_all];
+
 		return 0;
 	}
 
-	int is_size_valid()
+
+	void draw_vagon(int row, int column, int color, int val)
 	{
-		return (size1 > 0 && size2 > 0 && size_all > 0);
+		int ax, ay, bx, by, cx, cy, dx, dy;
+		ax = X0 + column * 80;
+		ay = Y0 + row * 80;
+		bx = ax;
+		by = ay + 40;
+		dx = ax + 40;
+		dy = ay;
+		cx = dx;
+		cy = by;
+
+		setcolor(color);
+
+		line(ax, ay, bx, by);
+		line(cx, cy, bx, by);
+		line(cx, cy, dx, dy);
+		setcolor(color+1);
+
+		char st[256];
+		sprintf(st, "%d", val);
+		outtextxy(ax + 2, ay + 15, st);
+	}
+
+	void connect_vagons(int row1, int col1, int row2, int col2, int color)
+	{
+		int x1 = X0 + col1 * 80 + 40;
+		int y1 = Y0 + row1 * 80 + 20;
+
+		int x2 = X0 + col2 * 80;
+		int y2 = Y0 + row2 * 80 + 20;
+
+		setcolor(color);
+		line(x1, y1, x1 + 20, y1);
+		line(x2 - 20, y2, x2, y2);
+
+	}
+
+	void draw_array(int* arr, int size, int main_clr, int con_clr, int* start_row, int* start_col)
+	{
+		int counter = 0;
+		int col = *start_col;
+		int row = *start_row;
+		int prev_col = 0; 
+		int prev_row = 0;
+		
+		for (int i = 0; i < size; i++)
+		{
+			draw_vagon(row, col, main_clr, arr[i]);
+			prev_col = col;
+			prev_row = row;
+
+			col++;
+			if (col == MAX_RL_NUM)
+			{
+				col = 0;
+				row++;
+			}
+
+			if (i != size - 1)
+			connect_vagons(prev_row, prev_col, row, col, con_clr);
+
+		}
+
+		*start_row= row + 1;	
+	}
+
+	void draw_array(clr_int* arr, int size, int main_clr, int con_clr, int* start_row, int* start_col)
+	{
+		int counter = 0;
+		int col = *start_col;
+		int row = *start_row;
+		int prev_col = 0;
+		int prev_row = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			draw_vagon(row, col, arr[i].clr, arr[i].val);
+			prev_col = col;
+			prev_row = row;
+
+			col++;
+			if (col == MAX_RL_NUM)
+			{
+				col = 0;
+				row++;
+			}
+
+			if (i != size - 1)
+				connect_vagons(prev_row, prev_col, row, col, con_clr);
+
+		}
+
+		*start_row = row + 1;
+	}
+
+	void draw_arrs()
+	{
+		int col = 0;
+		int row = 0;
+		draw_array(arr1, size1, 5, 12, &row, &col);
+		draw_array(arr2, size2, 8, 12, &row, &col);
+		draw_array(arr_all, size_all, 0, 12, &row, &col);
 	}
 };
 
@@ -219,35 +315,75 @@ void read_arr_from_db(marr_data* arrs)
 		exit(-1);
 	}
 
-	for(int idx = 0; res->next(); idx++)
+	for (int idx = 0; res->next(); idx++)
 	{
-		arrs->arr_all[idx] = res->getInt(1);
+		arrs->arr_all[idx].val = res->getInt(1);
 
-		if (idx  < arrs->size1)
-			arrs->arr1[idx] = arrs->arr_all[idx];
+		if (idx < arrs->size1)
+		{
+			arrs->arr1[idx] = arrs->arr_all[idx].val;
+			arrs->arr_all[idx].clr = 5;
+		}
 		else
-			arrs->arr2[idx - arrs->size1] = arrs->arr_all[idx];
+		{
+			arrs->arr2[idx - arrs->size1] = arrs->arr_all[idx].val;
+			arrs->arr_all[idx].clr = 8;
+		}
+	}
+}
+
+
+void write_result(marr_data* arrs)
+{
+	const char* server = "tcp://127.0.0.1:3306";
+	const char* username = "root";
+	const char* password = "11323";
+
+	sql::Driver* driver;
+	sql::Connection* con;
+	sql::Statement* stmt;
+	sql::ResultSet* res;
+
+	try
+	{
+		driver = get_driver_instance();
+		con = driver->connect(server, username, password);
+		con->setSchema("practice_task_5");	
+	}
+	catch (sql::SQLException e)
+	{
+		printf("Error message: %s", e.what());
+		system("pause");
+		exit(1);
 	}
 
+	stmt = con->createStatement();
+	stmt->execute("INSERT INTO result(val) VALUES (-555)");
+
+	for (int i = 0; i < arrs->size_all; i++)
+	{
+		char st[256];
+		sprintf(st, "INSERT INTO result(val) VALUES (%d)", arrs->arr_all[i].val);
+		
+		stmt->execute(st);
+	}
+
+	delete con;
+	delete stmt;
 }
 
 
 int main()
 {
+	marr_data * arrs = new marr_data;
+	read_arr_from_db(arrs);
+	arrs->sort_arr_all();
+
 	initwindow(1000, 700);
+	arrs->draw_arrs();
 	getch();
 	closegraph();
 
-
-	//marr_data * arrs = new marr_data;
-
-	//read_arr_from_db(arrs);
-	//arrs->print_arrs();
-
-	//arrs->sort_arr_all();
-	//arrs->print_arrs();
-
-
-
+	write_result(arrs);
 	return 0;
 }
