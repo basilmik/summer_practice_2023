f29e2b5c7b470265e714634b30760b2a26dc7157
basil <basil@BASILIK>
2023-07-13 19:49:57 +0300

    done task 6

--- task6/main.cpp
+++ task6/main.cpp
@@ -5,11 +5,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
 #include "../libwinbgi/src/graphics.h"
 
 
-#define TIME_DELTA 500
+#define TIME_DELTA 1000
+
 int get_random_in_range(int low, int up)
 {
 	if (up == 0 && low == 0)
@@ -42,43 +46,28 @@ public:
 class stack
 {
 public:
+	elem* top;
+	int col;
+	int last_placed;
 
 	void draw_stack_elem(int stack_idx, int elem_id, int val, int clr)
 	{
 		char str[128] = { 0 };
-		sprintf(str, "%d", val);
+		if (elem_id > 0)
+			sprintf(str, "%d %d", elem_id, val);
+		else
+			sprintf(str, "%d", val);
 		setcolor(clr);
 
 		outtextxy(stack_idx * 80, 800 - elem_id * 40, str);
 	}
 
 
-
-	/*void draw_full_stack(int _stack_idx)
-	{
-		elem* tmp = top;
-		int i = 0;
-		while (tmp != nullptr)
-		{
-			draw_stack_elem(_stack_idx, i, tmp->val);
-			tmp = tmp->prev;
-			i++;
-		}
-
-	}*/
-
-
-
-	elem* top;
-	int col;
-	int last_placed;
-
-
 	stack()
 	{
 		top = nullptr;
 		col = 0;
-		last_placed = 0;
+		last_placed = 1;
 	}
 
 	void push(int v)
@@ -88,7 +77,6 @@ public:
 		top = n_el;
 		draw_stack_elem(col, last_placed, v, 15);
 		last_placed++;
-		//printf("pushed: %d\n", v);
 	}
 
 	void cover(int _col, int _lp)
@@ -101,19 +89,17 @@ public:
 		rectangle(_col * 80, 800 - (_lp - 1) * 40, _col * 80 + 80, 800 - (_lp - 1) * 40 + 20);
 	}
 
-	int pop()
+	int pop(int _to_draw)
 	{
-		elem* n_top = top->prev;
-		
+		elem* n_top = top->prev;		
 		int val = top->val;
 		top = n_top;
-
-		cover(col, last_placed);
-
+		if (_to_draw) cover(col, last_placed);
 		last_placed--;
-
 		return val;
 	}
+
+
 
 	int is_empty()
 	{
@@ -128,7 +114,6 @@ public:
 			push(get_random_in_range(0,0));
 			Sleep(TIME_DELTA);
 		}
-
 	}
 
 	void print()
@@ -142,6 +127,49 @@ public:
 		printf("\n");
 	}
 
+	void save_to_table(char* _tname)
+	{
+		const char* server = "tcp://127.0.0.1:3306";
+		const char* username = "root";
+		const char* password = "11323";
+
+		sql::Driver* driver;
+		sql::Connection* con;
+		sql::Statement* stmt;
+		sql::ResultSet* res;
+
+		try
+		{
+			driver = get_driver_instance();
+			con = driver->connect(server, username, password);
+			con->setSchema("practice_task_6");
+		}
+		catch (sql::SQLException e)
+		{
+			printf("Error: %s", e.what());
+			exit(1);
+		}
+
+		stmt = con->createStatement();
+		char st[128] = { 0 };
+		sprintf(st, "DROP TABLE IF EXISTS %s", _tname);
+		stmt->execute(st);
+		char st2[128] = { 0 };
+		sprintf(st2, "CREATE TABLE IF NOT EXISTS %s (id serial primary key, value INTEGER)", _tname);
+		stmt->execute(st2);
+
+		while(!is_empty())
+		{
+			int val = pop(0);
+			char st[128] = { 0 };
+			sprintf(st, "INSERT INTO %s(value) VALUES (%d)", _tname, val);
+			stmt->execute(st);
+		}
+
+		delete con;
+		delete stmt;
+
+	}
 };
 
 void exchange(stack* st1, stack* st2)
@@ -156,7 +184,7 @@ void exchange(stack* st1, stack* st2)
 
 	if (!st1->is_empty())
 	{
-		val1 = st1->pop();
+		val1 = st1->pop(1);
 
 		is_valid1 = 1;
 		st1->cover(st1->col + 1, 1);
@@ -166,9 +194,9 @@ void exchange(stack* st1, stack* st2)
 	Sleep(TIME_DELTA);
 	if (!st2->is_empty())
 	{
-		val2 = st2->pop();
-		//Sleep(1500);
+		val2 = st2->pop(1);
 		is_valid2 = 1;
+
 		st2->cover(st2->col - 1, 1);
 		st2->draw_stack_elem(st2->col - 1, 0, val2, 12); // next to
 	}
@@ -206,7 +234,6 @@ void draw_exchange(stack* st1, stack* st2)
 	exchange(st1, st2);
 	st1->cover(st1->col + 1, 1);
 	st2->cover(st2->col - 1, 1);
-
 }
 
 int main()
@@ -220,25 +247,21 @@ int main()
 	int s2 = 0;
 
 	printf("size1 ");
-	scanf("%d", &s1);
+	while (s1 <= 0)
+		scanf("%d", &s1);
 
 	printf("size2 ");
-	scanf("%d", &s2);
+	while (s2 <= 0)
+		scanf("%d", &s2);
 
-
-	initwindow(1000, 1000);
+	initwindow(900, 900);
 
 	st1->random_fill(s1);	
-	st1->print();
-
 	st2->random_fill(s2);
-	st2->print();
 
 	draw_exchange(st1, st2);
-
-	st1->print();
-	
-	st2->print();
+	st1->save_to_table((char*)"s1");
+	st2->save_to_table((char*)"s2");
 
 	getch();
 	closegraph();
