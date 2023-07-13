353ef40756f2ba589ed817c49743b8896d44d6b3
basil <basil@BASILIK>
2023-07-13 23:18:24 +0300

    looks good

--- task_13/main.cpp
+++ task_13/main.cpp
@@ -1,9 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "stdio.h"
+#include <stdlib.h>
+#include <windows.h>
+
+#define ALL_EMPTY 0
+#define NOT_ALL_EMPTY 1
 
 class task
 {
 public:
-	int type; // 1 -- 3
+	int type; // 0 -- 2
 	task* prev;
 	task* next;
 
@@ -23,17 +30,22 @@ class stack
 public:
 	task* top_task;
 
+	bool is_empty()
+	{
+		return (top_task == nullptr);
+	}
+
 	stack()
 	{
 		top_task = nullptr;
-		for (int i = 0; i < 10; i++)
-		{
-			push(i % 3);
-		}
+		
 	}
 
 	int pop()
 	{
+		if (top_task == nullptr)
+			return -1;
+
 		task* t = top_task->next;
 		int type = top_task->type;
 		top_task = t;
@@ -43,6 +55,8 @@ public:
 
 	void push(int _type)
 	{
+		if (_type < 0 || _type > 2)
+			return;
 		task* t = new task(_type);
 		t->next = top_task;
 		top_task = t;
@@ -75,38 +89,33 @@ public:
 		last_task = nullptr;
 		size = 0;
 
-		for (int i = 0; i < 10; i++)
-		{
-			add(i % 3);
-		}
 	}
 
 
 	int get()
 	{
+		if (size == 0)
+			return -1;
+
 		task* t = top_task->next;
 		int type = top_task->type;
-		top_task = t;
+		
 		top_task->prev = nullptr;
+		top_task = t;
 		size--;
 		return type;
 	}
 
 	void add(int _type)
-	{
-		
+	{		
 		task* t = new task(_type);
-		t->prev = last_task;
-		
-		
+		t->prev = last_task;		
 		if (size == 0)
 		{
-			top_task = t;
-			
+			top_task = t;			
 		}
 		else
-		{
-			
+		{		
 			last_task->next = t;
 		}
 		last_task = t;
@@ -126,23 +135,24 @@ public:
 
 	}
 
-
+	bool is_empty()
+	{
+		return (size == 0);
+	}
 };
 
 
 class sys
 {
 public:
-	bool p1;
-	bool p2;
-	bool p3;
+	bool p[3];
 
 	stack* st;
 	queue* qu;
 
 	sys()
 	{
-		p1 = p2 = p3 = false;
+		p[0] = p[1] = p[2] = false;
 		st = new stack;
 		qu = new queue;
 	}
@@ -153,11 +163,98 @@ public:
 		qu->print();
 	}
 
+	int plan_stack()
+	{
+		int t_st = st->pop();
+		if (p[t_st] != true)
+		{
+			p[t_st] = true;
+			return t_st;
+		}
+		else
+		{
+			st->push(t_st);
+			return -1;
+		}
+	}
+
+	int plan() 
+	{
+		if (st->is_empty() && qu->is_empty())
+			return ALL_EMPTY;
+
+
+		int t = qu->get();
+		int t_st = -1;
+
+		if (t == -1) // qu is empty
+		{
+			t_st = plan_stack();
+		}
+		else // qu is not empty
+		{
+			if (p[t] == true) // this p is busy
+			{
+				// to stack
+				st->push(t);
+				t = -1;
+			}
+			else
+			{
+				// to p[t]
+				p[t] = true;
+				t_st = plan_stack();
+			}
+		
+		}
+		// clear all than not now placed
+
+		for (int i = 0; i < 3; i++)
+		{
+			if (i != t && i != t_st)
+				p[i] = false;
+		}
+
+		return NOT_ALL_EMPTY;
+	}
+
+	void print_stats()
+	{
+		//system("CLS");
+		printf("-----------------------\n");
+		st->print();
+		qu->print();
+		printf("%d %d %d\n", p[0], p[1], p[2]);
+		printf("-----------------------\n");
+	}
+
+
+	void emulate()
+	{
+		do
+		{
+			print_stats();
+			Sleep(2500);
+		} while (plan() != ALL_EMPTY);
+	}
+
+
 };
 
 int main()
 {
 	sys* msys = new sys;
+
+	int t = 0;
+	while (t != -1)
+	{
+		scanf("%d", &t);
+		if (t != -1)
+			msys->qu->add(t);
+	}
+
 	msys->print_all();
+	msys->emulate();
+
 	return 0;
 }
