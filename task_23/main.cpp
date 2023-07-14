402a521fcc8c1f298766a1d3d32255b5084e7cbc
basil <basil@BASILIK>
2023-07-14 23:19:24 +0300

    task_23 done

---
+++ task_23/main.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	int nv, ne;
+
+	scanf("%d %d", &nv, &ne);
+
+	const int inf = 1000;
+	std::vector<std::vector<int>> d(nv, std::vector<int>(nv, inf));
+	for (int i = 0; i < nv; i++)
+	{
+		d[i][i] = 0;
+	}
+	for (int i = 0; i < ne; i++)
+	{
+		int v1, v2;
+		scanf("%d %d", &v1, &v2);
+		v1--; v2--;
+		d[v1][v2] = 0;
+		d[v2][v1] = std::min(d[v2][v1], 1);
+	}
+	for (int k = 0; k < nv; k++)
+	{
+		for (int i = 0; i < nv; i++)
+		{
+			for (int j = 0; j < nv; j++)
+			{
+				d[i][j] = std::min(d[i][j], d[i][k] + d[k][j]);
+			}
+		}
+	}
+	int max = 0;
+	for (int i = 0; i < nv; i++)
+	{
+		for (int j = 0; j < nv; j++)
+		{
+			max = std::max(max, d[i][j]);
+		}
+	}
+	printf("%d", max);
+
+	return 0;
+}
